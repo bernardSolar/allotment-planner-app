5bfc0dbbdf1715c3fe1ca9e977c4cbf9eaaae97c
bernard <bernard.mccarty@gmail.com>
2025-03-15 09:51:43 +0000

    Add CalendarPage placeholder

---
+++ client/src/pages/CalendarPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CalendarPage = () => {
+  return (
+    <div className="container">
+      <h1>Planting Calendar</h1>
+      <p>This page will display a calendar of planting and gardening activities.</p>
+    </div>
+  );
+};
+
+export default CalendarPage;
