d4e08abea36a02db008a9e54822bb3b1db42a640
bernard <bernard.mccarty@gmail.com>
2025-03-15 09:51:29 +0000

    Add PlantDatabasePage placeholder

---
+++ client/src/pages/PlantDatabasePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlantDatabasePage = () => {
+  return (
+    <div className="container">
+      <h1>Plant Database</h1>
+      <p>This page will display a searchable database of plants.</p>
+    </div>
+  );
+};
+
+export default PlantDatabasePage;
