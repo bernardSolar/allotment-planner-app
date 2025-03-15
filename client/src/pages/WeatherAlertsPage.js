c0586ad5e8582df38adbcc6354b62abcd67aedbc
bernard <bernard.mccarty@gmail.com>
2025-03-15 09:51:50 +0000

    Add WeatherAlertsPage placeholder

---
+++ client/src/pages/WeatherAlertsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WeatherAlertsPage = () => {
+  return (
+    <div className="container">
+      <h1>Weather Alerts</h1>
+      <p>This page will display weather alerts and recommendations for your garden.</p>
+    </div>
+  );
+};
+
+export default WeatherAlertsPage;
